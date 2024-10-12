b1689ff7005184a93adbef49bc6bc6c2684b127a
gwisp2 <60121533+gwisp2@users.noreply.github.com>
2024-10-12 16:50:40 +0300

    Allow dragging windows

--- src/UI/WindowController.cs
+++ src/UI/WindowController.cs
@@ -35,6 +35,11 @@ public class WindowController(int id, string initialTitle, Action windowFunc, Re
     private void DrawWindow(int _)
     {
         windowFunc();
+        
+        // Allow dragging
+        GUI.DragWindow();
+        
+        // Don't pass any input to the rest of the game
         Vector2 mousePosition = UnityInput.Current.mousePosition;
         mousePosition.y = Screen.height - mousePosition.y;
         if (BlocksInput && IsMouseInWindow())
