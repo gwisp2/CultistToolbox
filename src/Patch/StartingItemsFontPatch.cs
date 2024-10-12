535c92636eed9502e86286329a5bf6658bfea9ec
gwisp2 <60121533+gwisp2@users.noreply.github.com>
2024-10-12 16:50:10 +0300

    Set font with icons when displaying starting items

---
+++ src/Patch/StartingItemsFontPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using MoMEssentials.UI;
+
+namespace MoMEssentials.Patch;
+
+[HarmonyPatch(typeof(StartingItem), nameof(StartingItem.Initialize))]
+public class StartingItemsFontPatch
+{
+    [HarmonyPostfix]
+    public static void Postfix(StartingItem __instance)
+    {
+        __instance.LabelTitle.trueTypeFont = IconFontLocator.IconFont;
+    }
+}
